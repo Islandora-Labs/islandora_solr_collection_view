9c3a938404db37bbf1e4df6ac308ffb42e9b6542
Rosie Le Faive <lefaive@gmail.com>
2015-08-07 14:00:14 -0300

    Use Teaser to template the row.

--- theme/islandora-solr-teaser.tpl.php
+++ theme/islandora-solr-teaser.tpl.php
@@ -8,17 +8,17 @@
  */
 ?>
 
-    <div class="islandora-basic-collection-object islandora-basic-collection-list-item clearfix">
+    <div class="islandora islandora-basic-collection-object islandora-basic-collection-list-item clearfix">
       <dl>
-        <dt> Hi! I'm a template!
+        <dt>
           <!-- Thumbnail -->
           <?php if (isset($values['PID'])): ?>
             <img src="/islandora/object/<?php print $values['PID']; ?>/datastream/TN/view" title="test"/>
           <?php endif; ?>
         </dt>
 
-               <dd class="collection-value">
-          <?php if (isset($values['thumb_link'])): ?>
+        <dd class="collection-value">
+          <?php if (isset($values['title_link'])): ?>
             <strong><?php print filter_xss($values['title_link']); ?></strong>
           <?php endif; ?>
         </dd>
